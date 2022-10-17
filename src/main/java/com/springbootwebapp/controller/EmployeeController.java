34a51303748b9b3867087307013b05e96a922fbf
silviney <silvineyf@gmail.com>
2022-10-17 20:02:27 -0300

    3º commit - Criando o Get List Employee e Insert Employee

--- src/main/java/com/springbootwebapp/controller/EmployeeController.java
+++ src/main/java/com/springbootwebapp/controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.springbootwebapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.springbootwebapp.entity.Employee;
 import com.springbootwebapp.repository.EmployeeRepository;
 
 @Controller
@@ -13,11 +18,32 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository eRepo;
 
-	@GetMapping({"/list","/"})
-	public ModelAndView getAllEmployees() {
+	// End point que mostra a tela inicial com a lista de employees que já foi
+	// cadastrada.
+
+	@GetMapping({ "/showEmployess", "/", "/list" })
+	public ModelAndView showEmployees() {
 		ModelAndView mav = new ModelAndView("list-employees");
-		mav.addObject("employees", eRepo.findAll());
+		List<Employee> list = eRepo.findAll();
+		mav.addObject("employees", list);
+		return mav;
+	}
+
+	// End point para Formulário de adicão de um novo employee - addEmployyeForm
+
+	@GetMapping({ "addEmployeeForm" })
+	public ModelAndView addEmployeeForm() {
+		ModelAndView mav = new ModelAndView("add-employee-form");
+		Employee newEmployee = new Employee();
+		mav.addObject("employee", newEmployee);
 		return mav;
 	}
 	
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute Employee employee) {
+		eRepo.save(employee);
+		return "redirect:/list";
+	}
+	
+
 }
--- src/main/java/com/springbootwebapp/entity/Employee.java
+++ src/main/java/com/springbootwebapp/entity/Employee.java
@@ -6,15 +6,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name="tbl_employees")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Employee {
 	
 	@Id
@@ -26,5 +19,48 @@ public class Employee {
 	private String email;
 	
 	private String department;
+	
+	public Employee() {
+	}
+	
+	public Employee(Long id, String name, String email, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.department = department;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 	
 }
