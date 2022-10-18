588b3ce5b42ee6ab4f46b24b9612ccdb792c6f5e
silviney <silvineyf@gmail.com>
2022-10-17 21:05:32 -0300

    5º commit - delete de Employee

--- src/main/java/com/springbootwebapp/repository/EmployeeRepository.java
+++ src/main/java/com/springbootwebapp/repository/EmployeeRepository.java
@@ -1,6 +1,9 @@
 package com.springbootwebapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springbootwebapp.entity.Employee;
@@ -8,4 +11,11 @@ import com.springbootwebapp.entity.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+	@Query(
+	        nativeQuery = true,
+	        value = "SELECT * " +
+	                "FROM tbl_employees " +
+	        		"ORDER BY name")
+	public List<Employee> findAll();
+	
 }
